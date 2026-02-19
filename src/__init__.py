32fb16f3fdffb4be2093c10151a41eb87f368ef8
Lucas Silva de Deus <silvadedeus.lucas@gmail.com>
2026-02-19 02:18:52 -0300

    chore: remove Azure cloud dependencies and clean up resources

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+Pacote: meu_pacote
+Descrição: Este pacote contém os módulos principais do projeto, incluindo
+modelos, serviços e utilitários.
+
+Autor: Lucas Silva de Deus
+Versão: 0.1.0
+Data de criação: 2026-02-19
+"""
